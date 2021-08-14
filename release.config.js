1b63fa632c6cd564af44dfbea1a3bd342e337b66
Aleksandar Ananiev <aleksandar.ananiev@mobilewaves.com>
2021-08-14 22:22:25 +0300

    update semantic release config

--- release.config.js
+++ release.config.js
@@ -1,17 +1,17 @@
 module.exports = {
     branches: "build",
-    repositoryURL: "https://github.com/camelotls/iwg-game-test-repo",
+    repositoryURL: "https://github.com/aaananiev/github-actions-test",
     plugins: [
       "@semantic-release/commit-analyzer",
       "@semantic-release/release-notes-generator",
-      [
-        "@semantic-release/github",
-        {
+      //[
+       // "@semantic-release/github",
+       // {
         //  assets: [
         //  { path: "build.zip", label: "Build" },
         //  { path: "package-lock.json", label: "Package Lock"}
         //  ],
-        },
-      ],
+        //},
+      //],
     ],
   };
