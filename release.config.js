3480f825db340b50775a9a91eb7986846eba10b6
Aleksandar Ananiev <aleksandar.ananiev@mobilewaves.com>
2021-08-14 23:06:44 +0300

    fix(game): release config

--- release.config.js
+++ release.config.js
@@ -1,18 +1,8 @@
 module.exports = {
-    branches: ["build"],
+    branches: "build",
     repositoryURL: "https://github.com/aaananiev/github-actions-test",
     plugins: [
       "@semantic-release/commit-analyzer",
       "@semantic-release/release-notes-generator"
-      //"@semantic-release/github"
-      //[
-       // "@semantic-release/github",
-       // {
-        //  assets: [
-        //  { path: "build.zip", label: "Build" },
-        //  { path: "package-lock.json", label: "Package Lock"}
-        //  ],
-        //},
-      //],
     ],
   };
