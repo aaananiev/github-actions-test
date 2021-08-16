3f659d56f7188064f618895382e7a60d1644418c
Aleksandar Ananiev <aleksandar.ananiev@mobilewaves.com>
2021-08-16 10:01:41 +0300

    update workflow

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: "build",
+    branches: "master",
     repositoryURL: "https://github.com/aaananiev/github-actions-test",
     plugins: [
       "@semantic-release/commit-analyzer",
